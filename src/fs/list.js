efeb31590926f0f56c86ae136c0af3fa7001a7d3
l1senochek <lisenochek96@gmail.com>
2024-01-26 11:04:55 +0300

    feat: add a solution for the list.js file

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import url from 'url';
+import path from 'path';
+import fs from 'fs/promises';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const errorMessage = 'FS operation failed';
+
+const folderPath = path.join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.access(folderPath);
+    const files = await fs.readdir(folderPath);
+
+    console.log('array of filenames:');
+    files.forEach((filename, index) =>
+      console.log(`${index + 1}) ${filename}`)
+    );
+  } catch {
+    throw new Error(errorMessage);
+  }
 };
 
 await list();
