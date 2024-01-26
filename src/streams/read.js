fec9fdbcca9b0006013f8dee5dd9a96accc7bb79
l1senochek <lisenochek96@gmail.com>
2024-01-26 13:33:25 +0300

    feat: add a solution for the read.js file

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import url from 'url';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+  readStream.on('data', (chunk) => process.stdout.write(chunk));
+  readStream.on('error', (error) => console.error(error.message));
 };
 
 await read();
