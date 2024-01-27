f3e7697f444f44c030ea9fe3bee6f4eb73b907ca
L1senochek <lisenochek96@gmail.com>
2024-01-27 17:58:48 +0300

    feat: add a solution for the transform.js file

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,13 @@
-const transform = async () => {
-    // Write your code here 
-};
+import stream from 'stream';
+
+const reverseTransform = new stream.Transform({
+  transform(chunk, _, callback) {
+    const reversedChunk = chunk.toString().split('').reverse().join('') + '\n';
+    callback(null, reversedChunk);
+  },
+});
+
+const transform = async () =>
+  process.stdin.pipe(reverseTransform).pipe(process.stdout);
 
 await transform();
