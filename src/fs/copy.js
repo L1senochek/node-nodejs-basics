641bab62c844900e8da022acceb4f9906bddfd03
L1senochek <lisenochek96@gmail.com>
2024-01-27 17:35:23 +0300

    feat: update copy.js file / add logic copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
-const copy = async () => {
-    // Write your code here 
-};
+import url from 'url'
+import path from 'path'
+import fs from 'fs/promises'
 
-await copy();
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const initialFolderPath = path.join(__dirname, 'files')
+const copyFolderPath = path.join(__dirname, 'files_copy')
+
+const errorMessage = 'FS operation failed'
+
+const copy = async () => {
+  try {
+    await fs.mkdir(copyFolderPath)
+    const files = await fs.readdir(initialFolderPath)
+
+    for (const file of files) {
+      const initialFilePath = path.join(initialFolderPath, file)
+      const copyFilePath = path.join(copyFolderPath, file)
+      const stats = await fs.stat(initialFilePath)
+
+      if (stats.isDirectory()) {
+        await copy(initialFilePath, copyFilePath)
+      } else {
+        await fs.copyFile(initialFilePath, copyFilePath)
+      }
+    }
+  } catch {
+    throw new Error(errorMessage)
+  }
+}
+
+await copy()
