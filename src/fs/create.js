860d5a83a8e250b03aafbd63c6fa130a1220d1ee
L1senochek <lisenochek96@gmail.com>
2024-01-27 17:34:31 +0300

    feat: update create.js file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
-const create = async () => {
-    // Write your code here 
-};
+import url from 'url'
+import path from 'path'
+import fs from 'fs/promises'
 
-await create();
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const filePath = path.join(__dirname, 'files', 'fresh.txt')
+
+const content = 'I am fresh and young'
+const errorMessage = 'FS operation failed'
+
+const create = async () => {
+  try {
+    await fs.writeFile(filePath, content, { flag: 'wx' })
+  } catch {
+    throw new Error(errorMessage)
+  }
+}
+
+await create()
