52c1c21184f010f0de4536baa66132ac7f40b73e
L1senochek <lisenochek96@gmail.com>
2024-01-27 17:47:47 +0300

    refactor: update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,20 +1,20 @@
-import url from 'url'
-import path from 'path'
-import fs from 'fs/promises'
+import url from 'url';
+import path from 'path';
+import fs from 'fs/promises';
 
-const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const filePath = path.join(__dirname, 'files', 'fresh.txt')
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
 
-const content = 'I am fresh and young'
-const errorMessage = 'FS operation failed'
+const content = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
 
 const create = async () => {
   try {
-    await fs.writeFile(filePath, content, { flag: 'wx' })
+    await fs.writeFile(filePath, content, { flag: 'wx' });
   } catch {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
-}
+};
 
-await create()
+await create();
