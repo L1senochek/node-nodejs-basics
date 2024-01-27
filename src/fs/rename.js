e4246b24412de6440013afcc0a1996366b9b70f7
L1senochek <lisenochek96@gmail.com>
2024-01-27 17:52:07 +0300

    refactor: update rename.js file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,25 +1,25 @@
-import url from 'url'
-import path from 'path'
-import fs from 'fs/promises'
+import url from 'url';
+import path from 'path';
+import fs from 'fs/promises';
 
-const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const errorMessage = 'FS operation failed'
+const errorMessage = 'FS operation failed';
 
-const wrongFilePath = path.join(__dirname, 'files', 'wrongFilename.txt')
-const properFilePath = path.join(__dirname, 'files', 'properFilename.md')
+const wrongFilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+const properFilePath = path.join(__dirname, 'files', 'properFilename.md');
 
 const rename = async () => {
   try {
-    await fs.access(wrongFilePath)
+    await fs.access(wrongFilePath);
     try {
-      await fs.access(properFilePath)
+      await fs.access(properFilePath);
     } catch {}
-    await fs.rename(wrongFilePath, properFilePath)
+    await fs.rename(wrongFilePath, properFilePath);
   } catch {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
-}
+};
 
-await rename()
+await rename();
