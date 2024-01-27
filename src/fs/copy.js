5689607bf4e5d1c55401f1da4b24477ccc418464
L1senochek <lisenochek96@gmail.com>
2024-01-27 17:46:45 +0300

    refactor: update copy.js file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,34 +1,34 @@
-import url from 'url'
-import path from 'path'
-import fs from 'fs/promises'
+import url from 'url';
+import path from 'path';
+import fs from 'fs/promises';
 
-const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const initialFolderPath = path.join(__dirname, 'files')
-const copyFolderPath = path.join(__dirname, 'files_copy')
+const initialFolderPath = path.join(__dirname, 'files');
+const copyFolderPath = path.join(__dirname, 'files_copy');
 
-const errorMessage = 'FS operation failed'
+const errorMessage = 'FS operation failed';
 
 const copy = async () => {
   try {
-    await fs.mkdir(copyFolderPath)
-    const files = await fs.readdir(initialFolderPath)
+    await fs.mkdir(copyFolderPath);
+    const files = await fs.readdir(initialFolderPath);
 
     for (const file of files) {
-      const initialFilePath = path.join(initialFolderPath, file)
-      const copyFilePath = path.join(copyFolderPath, file)
-      const stats = await fs.stat(initialFilePath)
+      const initialFilePath = path.join(initialFolderPath, file);
+      const copyFilePath = path.join(copyFolderPath, file);
+      const stats = await fs.stat(initialFilePath);
 
       if (stats.isDirectory()) {
-        await copy(initialFilePath, copyFilePath)
+        await copy(initialFilePath, copyFilePath);
       } else {
-        await fs.copyFile(initialFilePath, copyFilePath)
+        await fs.copyFile(initialFilePath, copyFilePath);
       }
     }
   } catch {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
-}
+};
 
-await copy()
+await copy();
